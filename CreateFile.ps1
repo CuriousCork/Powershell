2e452705c14137795c8c92ca63a2ad1421603649
CuriousCork <184484640+CuriousCork@users.noreply.github.com>
2024-11-27 18:31:21 +0000

    Update CreateFile.ps1
    adding indentation

--- CreateFile.ps1
+++ CreateFile.ps1
@@ -1,10 +1,9 @@
 Param(
     $Path
 )
-
 if(-Not $Path -eq ''){
-New-Item $Path
-Write-Host "O Ficheiro foi Criado"
+    New-Item $Path
+    Write-Host "O Ficheiro foi Criado"
 }
 Else {
     Write-Error "Erro!"
